a4c0837de7caf75d7f9f86650ca2c7fc89684638
Nathan Burgess <nathans.email.address@gmail.com>
2022-07-29 12:51:05 -0700

    second list added, an array of objects with a property that's an array of strings

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 import { renderGardenTools } from "./render-garden-tools.js";
 import { gardenTools } from "./garden-tools-array.js";
+import { renderCar } from "./render-cars.js";
+import { cars } from "./cars-array.js";
 const gardenToolListEl = document.getElementById('garden-tools-list');
+const carsListEl = document.getElementById('cars-list');
+
+
+let carCounter = 1;
 
 for (let gardenTool of gardenTools) {
     const newToolEl = renderGardenTools(gardenTool);
-    gardenToolListEl.appendChild(newToolEl);
+    gardenToolListEl.append(newToolEl);
 }
+
+for (let car of cars) {
+    const carHeader = document.createElement('h3');
+    carHeader.textContent = `Car ${carCounter}`;
+    carCounter++;
+    carsListEl.append(carHeader);
+    const newCarEl = renderCar(car);
+    console.log(newCarEl);
+    carsListEl.append(newCarEl);
+}
+
 
 // initialize global state
 
--- cars-array.js
+++ cars-array.js
@@ -3,19 +3,22 @@ export const cars = [
         make:'Volvo',
         model:'240',
         color:'gold',
-        mileage:'400,000'
+        mileage: '400,000',
+        previousOwners: ['Charlie', 'Bobby', 'Sam']
     },
     {
         make:'Honda',
         model:'Fit',
         color:'light blue',
-        mileage:'150,000'
+        mileage: '150,000',
+        previousOwners: ['Jennifer', 'Lily']
     },
     {
         make:'Toyota',
         model:'Camry',
         color:'green',
-        mileage:'110,000'
+        mileage: '110,000',
+        previousOwners: ['Max', 'Maxwell', 'Maximillion', 'Maxine']
     }
 ];
 
